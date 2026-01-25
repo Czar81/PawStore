4ab05829d5f931f80a4da3d9df19109793b47732
Czar <ianaaronza@gmail.com>
2026-01-24 21:06:51 -0600

    Add footer

---
+++ src/componets/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer>
+      <small>© PawStore 2025 — Todos los derechos reservados.</small>
+      <div className="social-media">
+        <a href="">Instagram</a>
+        <a href="">Facebook</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer
