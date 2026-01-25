272fccc543a4c4d8d3ecf9a46ab5f82886584179
Czar <ianaaronza@gmail.com>
2026-01-24 20:56:56 -0600

    Init app

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+import Navbar from './componets/Navbar';
+import Footer from './componets/Footer';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo officia
+          maiores aliquam. Tempore commodi ut nesciunt dolore ipsam obcaecati
+          quia esse ullam natus impedit itaque nobis dolores, inventore beatae
+          dolorum.
+        </p>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
