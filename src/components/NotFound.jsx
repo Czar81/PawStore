1ad3043f29d2ed0a768db07289debf000fe2dbcf
Czar <ianaaronza@gmail.com>
2026-01-26 15:24:46 -0600

    Add not found products

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import icon from '../assets/icons/emoji-sad.svg'
+
+function NotFound({title, desc}) {
+    return(
+        <>
+        <img className='emoji' src={icon} alt="Sad emoji" />
+        <p className='title'>{title}</p>
+        <p className='desc'>{desc}</p>
+        </>
+    )
+}
+
+export default NotFound
