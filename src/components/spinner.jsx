142bf54637d9b5e4a35ee2cc80ced53560bf2441
Czar <ianaaronza@gmail.com>
2026-01-26 14:39:24 -0600

    Styles spinner text

--- src/components/spinner.jsx
+++ src/components/spinner.jsx
@@ -2,7 +2,7 @@ function Spinner({ size = 40, text = 'Cargando...' }) {
   return (
     <>
       <div className="spinner" style={{ width: size, height: size }} />
-      <span className="sr-only">{text}</span>
+      <span className="spinner-text">{text}</span>
     </>
   );
 }
