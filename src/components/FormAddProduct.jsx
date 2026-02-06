1d8370b3c96aa203966b51de45f0704dfc1c1a50
Czar <ianaaronza@gmail.com>
2026-02-06 13:58:32 -0600

    Added basic verifications

--- src/components/FormAddProduct.jsx
+++ src/components/FormAddProduct.jsx
@@ -10,7 +10,11 @@ function FormAddPorduct() {
   return (
     <form className="form-products" onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="product-name">Nombre</label>
-      {errors.nombre && <span role="alert" className="alert-message">{errors.nombre.message}</span>}
+      {errors.nombre && (
+        <span role="alert" className="alert-message">
+          {errors.nombre.message}
+        </span>
+      )}
       <input
         type="text"
         name="nombre"
@@ -18,13 +22,17 @@ function FormAddPorduct() {
         className={`form-input ${errors.nombre ? 'input-error' : ''}`}
         placeholder="Nombre del producto"
         {...register('nombre', {
+          setValueAs: (value) => value.trim(),
+
           required: 'Campo obligatorio',
         })}
       />
 
       <label htmlFor="product-desc">Descripcion</label>
       {errors.descripcion && (
-        <span role="alert" className="alert-message">{errors.descripcion.message}</span>
+        <span role="alert" className="alert-message">
+          {errors.descripcion.message}
+        </span>
       )}
       <textarea
         name="descripcion"
@@ -32,16 +40,19 @@ function FormAddPorduct() {
         className={`form-input form-textarea ${errors.descripcion ? 'input-error' : ''}`}
         placeholder="Descripcion detallada del producto"
         {...register('descripcion', {
+          setValueAs: (value) => value.trim(),
           required: 'Campo obligatorio',
         })}
       ></textarea>
-      
+
       <div className="two-on-row">
         <div>
           <label htmlFor="product-price">Precio</label>
           {errors.precio && (
-        <span role="alert" className="alert-message">{errors.precio.message}</span>
-      )}
+            <span role="alert" className="alert-message">
+              {errors.precio.message}
+            </span>
+          )}
           <input
             type="number"
             name="precio"
@@ -50,15 +61,21 @@ function FormAddPorduct() {
             placeholder="0.00"
             {...register('precio', {
               required: 'Campo obligatorio',
+              setValueAs: (value) => value.trim(),
+              min: {
+                value: 0,
+                message: 'No puede ser negativo',
+              },
             })}
           />
-          
         </div>
         <div>
           <label htmlFor="product-category">Categoria</label>
           {errors.categoria && (
-        <span role="alert" className="alert-message">{errors.categoria.message}</span>
-      )}
+            <span role="alert" className="alert-message">
+              {errors.categoria.message}
+            </span>
+          )}
           <input
             type="text"
             name="categoria"
@@ -66,15 +83,17 @@ function FormAddPorduct() {
             className={`form-input ${errors.categoria ? 'input-error' : ''}`}
             placeholder="Categoria del producto (ej. Alimento, Juguetes)"
             {...register('categoria', {
+              setValueAs: (value) => value.trim(),
               required: 'Campo obligatorio',
             })}
           />
-          
         </div>
       </div>
       <label htmlFor="product-img">URL de la imagen</label>
       {errors.imagen && (
-        <span role="alert" className="alert-message">{errors.imagen.message}</span>
+        <span role="alert" className="alert-message">
+          {errors.imagen.message}
+        </span>
       )}
       <input
         type="text"
@@ -83,11 +102,21 @@ function FormAddPorduct() {
         className={`form-input ${errors.imagen ? 'input-error' : ''}`}
         placeholder="https://placehold.co/300x200.png?text=example"
         {...register('imagen', {
+          setValueAs: (value) => value.trim(),
           required: 'Campo obligatorio',
+          pattern: {
+            value: /^https?:\/\/.+/,
+            message: 'Debe ser una URL válida',
+          },
         })}
       />
-        
+
       <label htmlFor="product-stock">Stock</label>
+      {errors.stock && (
+        <span role="alert" className="alert-message">
+          {errors.stock.message}
+        </span>
+      )}
       <input
         type="number"
         name="stock"
@@ -95,12 +124,14 @@ function FormAddPorduct() {
         className={`form-input ${errors.stock ? 'input-error' : ''}`}
         placeholder="0"
         {...register('stock', {
+          setValueAs: (value) => value.trim(),
+          min: {
+            value: 0,
+            message: 'No puede ser negativo',
+          },
           required: 'Campo obligatorio',
         })}
       />
-      {errors.stock && (
-        <span role="alert" className="alert-message">{errors.stock.message}</span>
-      )}
       <button type="submit" className="btn-lilac">
         Agregar producto
       </button>
