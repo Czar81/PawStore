2f1288d93b46b850da3888168f86bfa6080b31a8
Czar <ianaaronza@gmail.com>
2026-02-07 18:00:31 -0600

    Add state product

---
+++ src/store/productStore.js
@@ -0,0 +1,35 @@
+import { create } from 'zustand';
+
+export const useProductStore = create((set, get) => ({
+  products: [],
+  selectedProductId: null,
+
+  setProducts: (products) => set({ products }),
+
+  setSelectedProductId: (id) => set({ selectedProductId: id }),
+
+  clearSelectedProductId: () => set({ selectedProductId: null }),
+  getSelectedProduct: () => {
+    const { products, selectedProductId } = get();
+    return products.find((product) => product.id === selectedProductId) || null;
+  },
+
+  addProduct: (product) =>
+    set((state) => ({
+      products: [...state.products, product],
+    })),
+
+  getProductById: (id) => get().products.find((product) => product.id === id),
+
+  updateProduct: (id, updatedData) =>
+    set((state) => ({
+      products: state.products.map((product) =>
+        product.id === id ? { ...product, ...updatedData } : product
+      ),
+    })),
+
+  deleteProduct: (id) =>
+    set((state) => ({
+      products: state.products.filter((product) => product.id !== id),
+    })),
+}));
