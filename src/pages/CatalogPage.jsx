25366aac49c11a6a2498bc3ee9aca99801753455
Czar <ianaaronza@gmail.com>
2026-02-07 19:05:20 -0600

    load products on app

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,34 +1,13 @@
-import { useEffect, useState } from 'react';
-import Spinner from '../components/generic/spinner';
 import ProductCard from '../components/product/ProductCard';
-import productsData from '../data/products.json';
 import NotFound from '../components/generic/NotFound';
 import { useProductStore } from '../store/productStore';
 
 function CatalogPage({ setActiveView }) {
-  const [loading, setLoading] = useState(true);
-
   const setSelectedProductId = useProductStore(
     (state) => state.setSelectedProductId
   );
   const products = useProductStore((state) => state.products);
-  const setProducts = useProductStore((state) => state.setProducts);
-  useEffect(() => {
-    async function loadProducts() {
-      setLoading(true);
-      setProducts(productsData);
-      setLoading(false);
-    }
-    loadProducts();
-  }, []);
 
-  if (loading) {
-    return (
-      <main className="main-text">
-        <Spinner size={48} text="Cargando productos..." />
-      </main>
-    );
-  }
   if (products.length === 0) {
     return (
       <main className="main-text">
