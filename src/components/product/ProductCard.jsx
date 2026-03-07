b383e03ca2e283222113809f67b3bbdbde48511f
Czar <ianaaronza@gmail.com>
2026-03-07 13:00:14 -0600

    Update product atribute

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,11 +1,17 @@
 function ProductCard({ product, onClick }) {
+  const productName = product.name || 'Sin nombre';
+  const productPrice = product.price || 0;
+  const productCategory = product.categoria || 'Sin categoría';
+  const productImage =
+    product.image || 'https://via.placeholder.com/300x200?text=No+Image';
+
   return (
     <article className="product-card">
-      <img src={product.imagen} alt="Image product" />
+      <img src={productImage} alt={productName} />
       <div className="description">
-        <h3>{product.nombre}</h3>
-        <p className="price">₡{product.precio}</p>
-        <p className="category">{product.categoria}</p>
+        <h3>{productName}</h3>
+        <p className="price">₡{productPrice}</p>
+        <p className="category">{productCategory}</p>
         <button className="btn-lilac" type="button" onClick={onClick}>
           Ver detalles
         </button>
