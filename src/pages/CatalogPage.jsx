68490e25a438a642edeba4fe3331b3e245d33df1
Czar <ianaaronza@gmail.com>
2026-03-07 12:14:01 -0600

    Normalized data prodcut

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,6 +1,6 @@
-import ProductCard from '../components/product/ProductCard';
-import NotFound from '../components/generic/NotFound';
-import { useProductStore } from '../store/productStore';
+import ProductCard from '@/components/product/ProductCard';
+import NotFound from '@/components/generic/NotFound';
+import { useProductStore } from '@/store/productStore';
 
 function CatalogPage({ setActiveView }) {
   const setSelectedProductId = useProductStore(
@@ -8,21 +8,34 @@ function CatalogPage({ setActiveView }) {
   );
   const products = useProductStore((state) => state.products);
 
-  if (products.length === 0) {
+  const normalizedProducts = products.map((product) => ({
+    ...product,
+    id: product.id_product,
+    nombre: product.name ,
+    precio: product.price,
+    imagen:
+      product.image ||
+      'https://via.placeholder.com/300x200?text=No+Image',
+    categoria: product.categoria || 'Sin categoría',
+    stock: product.amount|| 0,
+  }));
+
+  if (normalizedProducts.length === 0) {
     return (
       <main className="main-text">
         <NotFound
           title="No hay productos disponibles por el momento"
-          desc="Intenta cambiar los filtros o la busqueda"
+          desc="Intenta cambiar los filtros o la búsqueda"
         />
       </main>
     );
   }
+
   return (
     <main className="catalog">
       <h2>Catalogo de Productos</h2>
       <div className="products-container">
-        {products.map((product) => (
+        {normalizedProducts.map((product) => (
           <ProductCard
             product={product}
             key={product.id}
